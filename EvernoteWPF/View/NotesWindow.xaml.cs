3fa3e70db97cc9d4a451783c3a7bc0db918be004
Diego <diego_sousa.s@outlook.com>
2023-07-01 12:25:52 -0300

    Implementando reconhecimento de fala

--- EvernoteWPF/View/NotesWindow.xaml.cs
+++ EvernoteWPF/View/NotesWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Windows;
 using System.Windows.Documents;
 
 namespace EvernoteWPF.View
@@ -18,9 +20,22 @@ namespace EvernoteWPF.View
             Application.Current.Shutdown();
         }
 
-        private void SpeechButton_Click(object sender, RoutedEventArgs e)
+        private async void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
+            string region = "eastus";
+            string key = "fef5e4c5d27c43d5a8495e78e671463d";
 
+            var speechConfig = SpeechConfig.FromSubscription(key, region);
+
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+                {
+                    var result = await recognizer.RecognizeOnceAsync();
+                    contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
+                }
+            }
         }
 
         private void contentRichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
